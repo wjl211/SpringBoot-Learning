e621bfc4d3d7f73119e33486b997afa5d452faf1
wjl211 <329968169@qq.com>
2018-08-29 21:55:29 +0800

    init project

--- springboot-03-logging/src/main/java/com/atguigu/springboot03logging/Springboot03LoggingApplication.java
+++ springboot-03-logging/src/main/java/com/atguigu/springboot03logging/Springboot03LoggingApplication.java
@@ -3,6 +3,7 @@ package com.atguigu.springboot03logging;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Springboot03LoggingApplication {
 
